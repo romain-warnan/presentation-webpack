71938a52f0776b567dd50ffce0dd5b4134de35ef
Renaud Genevois <renaud.genevois@insee.fr>
2017-11-17 15:40:18 +0100

    restructuration

--- seminaire2017-front-end/src/actions/app-actions.js
+++
@@ -1,7 +0,0 @@
-export const START_WAITING = "__START_WAITING__";
-export const STOP_WAITING = "__STOP_WAITING__";
-export const SET_NAV_INDEX = "__SET_NAV_INDEX__";
-
-export const startWaiting = () => ({ type: START_WAITING });
-export const stopWaiting = () => ({ type: STOP_WAITING });
-export const setNavIndex = index => ({ type: SET_NAV_INDEX, index });
--- seminaire2017-front-end/src/components/preloader/preloader-container.js
+++
@@ -1,4 +0,0 @@
-import { connect } from "react-redux";
-import Preloader from "./preloader";
-
-export default connect(state => ({ active: state.appReducer.waiting }))(Preloader);
